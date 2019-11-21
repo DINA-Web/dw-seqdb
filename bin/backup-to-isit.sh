69c6d5ef6255a98f70293e0d311e446a99848a34
inkimar e <inkimar@gmail.com>
2019-11-21 15:29:57 +0100

    [PROD] working sample of pg_dump

--- bin/backup-to-isit.sh
+++ bin/backup-to-isit.sh
@@ -2,6 +2,8 @@
 source ../env/.env.postgres
 NOW=`date +"%Y-%m-%d"`
 TARGET=../db-backup
-docker exec db sh -c 'exec pg_dump -U ${POSTGRES_USER} -w -F t ${POSTGRES_DB}' > ${TARGET}/${POSTGRES_DB}_${NOW}.sql.tar
+# docker exec shared_db sh -c 'exec pg_dump -U ${POSTGRES_USER} -w -F t ${POSTGRES_DB}' > ${TARGET}/${POSTGRES_DB}_${NOW}.sql.tar
+# docker exec shared_db sh -c 'exec pg_dump -U ${POSTGRES_USER}  ${POSTGRES_DB}' > ${TARGET}/${POSTGRES_DB}_${NOW}.sql
+docker exec shared_db sh -c 'exec  pg_dump -U postgres seqdb_prod' > ${TARGET}/${POSTGRES_DB}_${NOW}.sql
 echo $NOW >> biobackup.log
-scp ${TARGET}/${MYSQL_DATABASE}_${NOW}.sql biobackup:/backup/SEQDB &>>biobackup.log
+scp ${TARGET}/${POSTGRES_DB}_${NOW}.sql biobackup.nrm.se:/backup/SEQDB &>>biobackup.log

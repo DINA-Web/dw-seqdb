7260c84489132d79122729d7cd8d77b20aa3608f
inkimar e <inkimar@gmail.com>
2020-04-06 17:05:50 +0200

    backup-script

--- bin/postgres_backup.sh
+++ bin/postgres_backup.sh
@@ -6,4 +6,4 @@ TARGET=../db-backup
 # docker exec shared_db sh -c 'exec pg_dump -U ${POSTGRES_USER}  ${POSTGRES_DB}' > ${TARGET}/${POSTGRES_DB}_${NOW}.sql
 docker exec shared_db sh -c 'exec  pg_dump -U postgres seqdb_prod' > ${TARGET}/${POSTGRES_DB}_${NOW}.sql
 echo $NOW >> biobackup.log
-scp ${TARGET}/${POSTGRES_DB}_${NOW}.sql biobackup:/backup/SEQDB &>>biobackup.log
+scp ${TARGET}/${POSTGRES_DB}_${NOW}.sql biobackup.nrm.se:/backup/SEQDB &>>biobackup.log
